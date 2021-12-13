ef9c6cf0a01f55a89821d5bc1702d42464937b77
Bruno Marra <bruno.marra@cinnecta.com>
2021-12-13 11:14:53 -0300

    corrige bugs do script. ainda está ruim

--- Grafana/elasticsearch2elastic.py
+++ Grafana/elasticsearch2elastic.py
@@ -12,7 +12,8 @@ interval = int(os.environ.get('ES_METRICS_INTERVAL', '60'))
 
 # ElasticSearch Cluster to Send Metrics
 elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', 'elasticsearch_metrics')
-elasticMonitoringCluster = os.environ.get('ES_METRICS_MONITORING_CLUSTER_URL', 'http://server2:9200')
+elasticMonitoringCluster = os.environ.get(
+    'ES_METRICS_MONITORING_CLUSTER_URL', 'http://server2:9200')
 
 # Enable Elasticsearch Security
 # read_username and read_password for read ES cluster information
@@ -25,34 +26,38 @@ write_es_security_enable = False
 write_username = "write_username"
 write_password = "write_password"
 
+
 def handle_urlopen(urlData, read_username, read_password):
-    if read_es_security_enable: 
-      try:
-	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        password_mgr.add_password(None, urlData, read_username, read_password)
-        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-        opener = urllib2.build_opener(handler)
-        urllib2.install_opener(opener)
-        response = urllib2.urlopen(urlData)
-	return response
-      except Exception as e:
-        print "Error:  {0}".format(str(e))
+    if read_es_security_enable:
+        try:
+            password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            password_mgr.add_password(
+                None, urlData, read_username, read_password)
+            handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+            opener = urllib2.build_opener(handler)
+            urllib2.install_opener(opener)
+            response = urllib2.urlopen(urlData)
+            return response
+        except Exception as e:
+            print "Error:  {0}".format(str(e))
     else:
-      try:
-        response = urllib2.urlopen(urlData)
-        return response
-      except Exception as e:
-        print "Error:  {0}".format(str(e))
+        try:
+            response = urllib2.urlopen(urlData)
+            return response
+        except Exception as e:
+            print "Error:  {0}".format(str(e))
+
 
 def fetch_clusterhealth():
     try:
         utc_datetime = datetime.datetime.utcnow()
         endpoint = "/_cluster/health"
         urlData = elasticServer + endpoint
-        response = handle_urlopen(urlData,read_username,read_password)
+        response = handle_urlopen(urlData, read_username, read_password)
         jsonData = json.loads(response.read())
         clusterName = jsonData['cluster_name']
-        jsonData['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+        jsonData['@timestamp'] = str(
+            utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
         if jsonData['status'] == 'green':
             jsonData['status_code'] = 0
         elif jsonData['status'] == 'yellow':
@@ -71,9 +76,10 @@ def fetch_clusterstats():
     utc_datetime = datetime.datetime.utcnow()
     endpoint = "/_cluster/stats"
     urlData = elasticServer + endpoint
-    response = handle_urlopen(urlData,read_username,read_password)
+    response = handle_urlopen(urlData, read_username, read_password)
     jsonData = json.loads(response.read())
-    jsonData['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+    jsonData['@timestamp'] = str(
+        utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
     post_data(jsonData)
 
 
@@ -81,16 +87,17 @@ def fetch_nodestats(clusterName):
     utc_datetime = datetime.datetime.utcnow()
     endpoint = "/_cat/nodes?v&h=n"
     urlData = elasticServer + endpoint
-    response = handle_urlopen(urlData,read_username,read_password)
+    response = handle_urlopen(urlData, read_username, read_password)
     nodes = response.read()[1:-1].strip().split('\n')
     for node in nodes:
         endpoint = "/_nodes/%s/stats" % node.rstrip()
         urlData = elasticServer + endpoint
-        response = handle_urlopen(urlData,read_username,read_password)
+        response = handle_urlopen(urlData, read_username, read_password)
         jsonData = json.loads(response.read())
         nodeID = jsonData['nodes'].keys()
         try:
-            jsonData['nodes'][nodeID[0]]['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+            jsonData['nodes'][nodeID[0]]['@timestamp'] = str(
+                utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
             jsonData['nodes'][nodeID[0]]['cluster_name'] = clusterName
             newJsonData = jsonData['nodes'][nodeID[0]]
             post_data(newJsonData)
@@ -102,9 +109,10 @@ def fetch_indexstats(clusterName):
     utc_datetime = datetime.datetime.utcnow()
     endpoint = "/_stats"
     urlData = elasticServer + endpoint
-    response = handle_urlopen(urlData,read_username,read_password)
+    response = handle_urlopen(urlData, read_username, read_password)
     jsonData = json.loads(response.read())
-    jsonData['_all']['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+    jsonData['_all']['@timestamp'] = str(
+        utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
     jsonData['_all']['cluster_name'] = clusterName
     post_data(jsonData['_all'])
 
@@ -112,14 +120,15 @@ def fetch_indexstats(clusterName):
 def post_data(data):
     utc_datetime = datetime.datetime.utcnow()
     url_parameters = {'cluster': elasticMonitoringCluster, 'index': elasticIndex,
-        'index_period': utc_datetime.strftime("%Y.%m.%d"), }
+                      'index_period': utc_datetime.strftime("%Y.%m.%d"), }
     url = "%(cluster)s/%(index)s-%(index_period)s/message" % url_parameters
     headers = {'content-type': 'application/json'}
     try:
         req = urllib2.Request(url, headers=headers, data=json.dumps(data))
         if write_es_security_enable:
             password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-            password_mgr.add_password(None, url, write_username, write_password)
+            password_mgr.add_password(
+                None, url, write_username, write_password)
             handler = urllib2.HTTPBasicAuthHandler(password_mgr)
             opener = urllib2.build_opener(handler)
             urllib2.install_opener(opener)
